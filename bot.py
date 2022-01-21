f31007f8aad5d4e7fd726892d8bfbaa89932ce13
DanielPikilidis <danielpikilidis@mail.com>
2022-01-21 11:21:24 +0200

    Remove a forgotten line

--- bot.py
+++ bot.py
@@ -1,11 +1,8 @@
 import discord, os, json
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.members = True
-
 prefix = "?/"
-bot = commands.Bot(command_prefix=prefix, help_command=None, intents=intents)
+bot = commands.Bot(command_prefix=prefix, help_command=None)
 
 started = False
 couriers = []
